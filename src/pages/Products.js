bfb8f731854c4083f2ecd2b1881d50fc63d9d33d
Juan Camilo <jccifuentes21@gmail.com>
2022-08-24 10:07:43 -0700

    added switch and fixed the links to product details

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <>
       <h1>The Products page</h1>
       <ul>
-        <li>Product 1</li>
-        <li>Product 2</li>
-        <li>Product 3</li>
+        <li><Link to='/products/p1'>A keyboard</Link> </li>
+        <li><Link to='/products/p2'>A golf club</Link>  </li>
+        <li><Link to='/products/p3'>A tv</Link> </li>
       </ul>
     </>
   );
