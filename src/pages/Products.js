9e74aacfd129c93e4d848e0c848acb42fef2abd2
Juan Camilo <jccifuentes21@gmail.com>
2022-08-24 10:00:11 -0700

    first commit

---
+++ src/pages/Products.js
@@ -0,0 +1,14 @@
+const Products = () => {
+  return (
+    <>
+      <h1>The Products page</h1>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </>
+  );
+};
+
+export default Products;
